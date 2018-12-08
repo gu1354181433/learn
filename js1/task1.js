9a52e470d149ee3e0bee6e5c22c3af4f35cd14e0
小银 <44015032+gu1354181433@users.noreply.github.com>
2018-12-08 17:30:48 +0800

    JS1
    12.07-1

--- js1/task1.js
+++ js1/task1.js
@@ -1,82 +1,92 @@
-
-
-
-
-
+var y = "#ffa500";
 function startBtn() {
-    var x = Math.random()*10;
-    var y = Math.round(x);
-	console.log(y);
-	for (;y==10||y==0;) {
-		var x = Math.random()*10;
-		var y = Math.round(x);
-        console.log(y);
-    }
-    var a = Math.random()*10;
-    var b = Math.round(a);
-	console.log(b);
-	for (;b==10||b==0;) {
-		var a = Math.random()*10;
-		var b = Math.round(a);
-        console.log(b);
-    }
-    var e = Math.random()*10;
-    var d = Math.round(e);
-	console.log(d);
-	for (;d==10||d==0;) {
-		var e = Math.random()*10;
-		var d = Math.round(e);
-        console.log(d);
-    }
-    for(;y==b||y==d;){
-        var x = Math.random()*10;
-        var y = Math.round(x);
-        console.log(y);
-        for (;y==10||y==0;) {
-            var x = Math.random()*10;
-            var y = Math.round(x);
-            console.log(y);
+    
+   function randomx() { 
+       var x = 0;
+       for(;x==10||x==0;) {
+            var x = Math.round(Math.random()*10);
+       }
+       return x;  
+   }
+   var Arr = [];
+   while(Arr.length<3){
+       var aa    = "grid"+randomx();
+       var onoff = true;
+       for(var i=0;i<Arr.length;i++) {
+           if (Arr[i]==aa){
+               onoff = false;
+               break;
+           }
+           else {
+               onoff = true;
+           }
+           
+           console.log(Arr[i]);
+       }
+       console.log(onoff);
+       if(onoff){
+           Arr.push(aa);
+       }
+   }
+   for(var c=1;c<10;c++) {
+       var                     b                        = "grid"+c;
+       document.getElementById(b).style.backgroundColor = y;
+   }
+   function colorF(){
+       var color = "#";
+       for(var s=0;s<3;s++){
+            var r = Math.round(Math.random()*225);
+            if(r<=15){
+                var color = color+"0"+r.toString(16);
+            }
+            else{
+                var color = color+r.toString(16);
+            }
+            console.log(color);
         }
+        return color;
     }
-    for(;b==d||b==y;){
-        var a = Math.random()*10;
-        var b = Math.round(a);
-	console.log(b);
-	for (;b==10||b==0;) {
-		var a = Math.random()*10;
-		var b = Math.round(a);
-        console.log(b);
-    }
-    }
-    var                     z                              = "grid"+y;
-    var                     c                              = "grid"+b;
-    var                     f                              = "grid"+d;
-    document.getElementById("grid1").style.backgroundColor = "#ffa500";
-    document.getElementById("grid2").style.backgroundColor = "#ffa500";
-    document.getElementById("grid3").style.backgroundColor = "#ffa500";
-    document.getElementById("grid4").style.backgroundColor = "#ffa500";
-    document.getElementById("grid5").style.backgroundColor = "#ffa500";
-    document.getElementById("grid6").style.backgroundColor = "#ffa500";
-    document.getElementById("grid7").style.backgroundColor = "#ffa500";
-    document.getElementById("grid8").style.backgroundColor = "#ffa500";
-    document.getElementById("grid9").style.backgroundColor = "#ffa500";
-    document.getElementById(z).style.backgroundColor       = "red";
-    document.getElementById(c).style.backgroundColor       = "red";
-    document.getElementById(f).style.backgroundColor       = "red";
+   var colorS = [];
+   while(colorS.length<3){
+       var xx    = y;
+       var coBln = true;
+       for(;xx==y;){
+            var xx = colorF();
+       }
+       for(var n=0;n<colorS.length;n++){
+           if(colorS[n]==xx){
+               var coBln = false;
+               break;
+           }
+           else{
+               var coBln = true;
+           }
+       }
+       if(coBln){
+           colorS.push(xx);
+       }
+       console.log(colorS)
+   }
+   
+   for(var a=0;a<3;a++) {
+    document.getElementById(Arr[a]).style.background = colorS[a];
+    console.log(Arr);
+    console.log(Arr[a]);    
+   }
+
+}
+var stop;
+function Start(){
+    clearInterval(stop);
+   stop = setInterval('startBtn()',1000);
 }
 function stopBtn(){
-    document.getElementById("grid1").style.backgroundColor = "#ffa500";
-    document.getElementById("grid2").style.backgroundColor = "#ffa500";
-    document.getElementById("grid3").style.backgroundColor = "#ffa500";
-    document.getElementById("grid4").style.backgroundColor = "#ffa500";
-    document.getElementById("grid5").style.backgroundColor = "#ffa500";
-    document.getElementById("grid6").style.backgroundColor = "#ffa500";
-    document.getElementById("grid7").style.backgroundColor = "#ffa500";
-    document.getElementById("grid8").style.backgroundColor = "#ffa500";
-    document.getElementById("grid9").style.backgroundColor = "#ffa500";
-    
+    clearInterval(stop);
+    for(var c=1;c<10;c++) {
+        var                     b                        = "grid"+c;
+        document.getElementById(b).style.backgroundColor = y;
+    }
 }
-
 
 
 
