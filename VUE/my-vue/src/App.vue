62d03a293a96389f794f372e6e42c0fe8e4e6871
小银 <44015032+gu1354181433@users.noreply.github.com>
2019-04-23 15:59:50 +0800

    1

--- VUE/my-vue/src/App.vue
+++ VUE/my-vue/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" f='<span style="color:red" >hello</span>'/>
+    <test></test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test'
 
 export default {
   name      : 'app',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
---
+++ VUE/my-vue/src/components/Test.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="test">
+    <!--  获取数据  -->
+        <h1>Test{{title}}</h1>
+        <p>{{user.firstName}}</p>
+        <p v-text="user.lestName" ></p>
+        <!-- v-If -->
+        <p v-if="showName" >{{user.firstName}}</p>
+        <!-- v-else -->
+        <p v-else>13213123 </p>
+        <!-- v-for -->
+        <ul>
+            <li v-for="item in Items" :key="item.id">{{item.title}}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'test',
+    data(){
+        return{
+            title: 'Hello Vue.js!',
+            user : {
+                firstName: "Hivana",
+                lestName : "Dva"
+            },
+            showName: false,
+            Items   : [
+                {title:"item1"},
+                {title:"item2"},
+                {title:"item3"}
+            ]
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
